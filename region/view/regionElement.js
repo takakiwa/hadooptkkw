5cbf15e4027206fb1ba86eacb3b48cf84bcb7f3c
Tatsuya Takakiwa <tkty4325@jcom.home.ne.jp>
2012-09-05 20:09:34 +0900

    905 region

---
+++ region/view/regionElement.js
@@ -0,0 +1,18 @@
+wgp.RegionElement = Backbone.Model.extend({
+	defaults : {
+		objectId : null,
+		objectName : null,
+		pointX : null,
+		pointY : null,
+		width : null,
+		height : null,
+		zIndex : null,
+		URL : null,
+		text : null
+	},
+	idAttribute : "objectId"
+});
+
+var MapElementList = Backbone.Collection.extend({
+	model : wgp.RegionElement
+});
